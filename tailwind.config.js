8c530ad8769f31623f73368e524f9c2a64d38658
aliii390 <alli.said.errahmani@gmail.com>
2024-11-05 12:27:28 +0100

    ADD: setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+
+      backgroundImage : {
+        'bg-masthead': "url('./Take-assets/masthead.jpg')",
+        'bg-lg': "url('./Take-assets/box-bg-lg.jpg')",
+        'bg-box': "url('./Take-assets/box-bg.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
+
